43356c56e8068f4648f59d6cdfa45169302225fe
Akash <akash.mistry@innodeed.com>
2023-11-02 03:00:28 +0530

    changed Icons

--- src/components/Week.js
+++ src/components/Week.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { habitDone, habitUnDone, habitNone } from "../Redux/habitSlice";
 const Week = ({ week }) => {
   const today = new Date();
@@ -46,31 +46,71 @@ const Week = ({ week }) => {
         <p>{week.isDone}</p>
         <div className="flex flex-col">
           <div>
-            <input
-              type="radio"
-              name="radio-2"
-              className="radio radio-success"
-              onClick={() => markToDone(week.id)}
-            />{" "}
-            <small className="mb-1">Done</small>
-          </div>
-          <div>
-            <input
-              type="radio"
-              name="radio-2"
-              className="radio radio-error"
-              onClick={() => markToUnDone(week.id)}
-            />{" "}
-            <small className="mb-1">Not Done</small>
-          </div>
-          <div>
-            <input
-              type="radio"
-              name="radio-2"
-              className="radio radio-info"
-              onClick={() => markToNone(week.id)}
-            />{" "}
-            <small className="mb-1">Leave</small>
+            <i onClick={() => markToDone(week.id)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={
+                  week.isDone === true
+                    ? "w-6 h-6 fill-green-600"
+                    : "w-6 h-6 stroke-green-600"
+                }
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </i>
+            <i onClick={() => markToUnDone(week.id)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={
+                  week.isDone === false
+                    ? "w-6 h-6 fill-red-600"
+                    : "w-6 h-6 stroke-red-600"
+                }
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </i>
+            <i onClick={() => markToNone(week.id)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={
+                  week.isDone === ""
+                    ? "w-6 h-6 fill-amber-600"
+                    : "w-6 h-6 stroke-amber-600"
+                }
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 019 14.437V9.564z"
+                />
+              </svg>
+            </i>
           </div>
         </div>
       </div>
